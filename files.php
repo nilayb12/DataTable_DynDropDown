45b5ffb9a1b6c43a262d1a3b8a2ce013cb2ef995
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2023-12-06 16:23:40 +0530

    Modification for Filename display

--- files.php
+++ files.php
@@ -4,11 +4,13 @@ echo '<select id="selectFile">';
 // echo '<option selected disabled>Choose Data Source</option>';
 $files = glob('{' . $data . '}/*.csv', GLOB_BRACE);
 $flen = 1;
+$dlen = strlen($data);
 if (empty($files)) {
-    echo '<option value="Empty" selected disabled>Empty</option>';
+    echo '<option value="Empty" selected disabled>No Data</option>';
 } else {
     foreach ($files as $file) {
-        echo '<option value="' . $file . '">' . substr($file, strlen($data) + 1, 10) . '</option>';
+        echo '<option value="' . $file . '">' . substr($file, $dlen + 1, strpos($file, '.') - $dlen - 1) . '</option>';
+        // strtok($file, '.')
         $flen += 1;
     }
 }
