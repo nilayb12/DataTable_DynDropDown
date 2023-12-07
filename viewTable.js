35fe80aa226cf7bf5d025471f321b5a5f9ef47bb
Nilay Baranwal <65295315+nilayb12@users.noreply.github.com>
2023-12-07 13:21:42 +0530

    Added Option for Datatable DOM Modification

--- viewTable.js
+++ viewTable.js
@@ -102,6 +102,9 @@ function initDataTable() {
         scrollY: '75vh',
         scrollCollapse: true,
         dom: 'PlBfritp',
+        // dom: 'P'+
+        // '<"fg-toolbar ui-toolbar ui-widget-header ui-helper-clearfix ui-corner-tl ui-corner-tr"lBfr>'+'t'+
+        // '<"fg-toolbar ui-toolbar ui-widget-header ui-helper-clearfix ui-corner-bl ui-corner-br"ip>',
         processing: true,
         pagingType: "full_numbers",
         keys: true,
@@ -123,7 +126,7 @@ function initDataTable() {
             },
             {
                 extend: 'csv',
-                text: '<i class="fa-solid fa-file-csv" />',
+                text: '<i class="fa-solid fa-file-csv fa-xl" />',
                 title: csvFileSelected.value.substr(folderSelected.value.length,
                     csvFileSelected.value.length - folderSelected.value.length - 4),
                 titleAttr: 'Download Data as CSV',
